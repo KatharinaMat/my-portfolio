859f063816e4f32cbaada196314ea0cde5348405
KatharinaMat <katharina.matkevits@gmail.com>
2026-03-13 22:03:24 +0200

    favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,34 @@
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/i18n'],
-  css: ['~/assets/css/main.css'],
+  modules: ["@nuxtjs/i18n"],
+  css: ["~/assets/css/main.css"],
+
+  app: {
+    head: {
+      link: [
+        {
+          rel: "icon",
+          type: "image/x-icon",
+          href: "/favicon.ico",
+        },
+      ],
+    },
+  },
 
   i18n: {
-    strategy: 'prefix',
-    defaultLocale: 'en',
+    strategy: "prefix",
+    defaultLocale: "en",
     lazy: true,
-    langDir: 'locales',
+    langDir: "locales",
     locales: [
-      { code: 'en', file: 'en.json', name: 'English' },
-      { code: 'et', file: 'et.json', name: 'Eesti' }
+      { code: "en", file: "en.json", name: "English" },
+      { code: "et", file: "et.json", name: "Eesti" },
     ],
 
     compilation: {
       strictMessage: false,
-      escapeHtml: false
+      escapeHtml: false,
     },
 
-    vueI18n: './i18n/i18n.config.ts'
-  }
-})
+    vueI18n: "./i18n/i18n.config.ts",
+  },
+});
